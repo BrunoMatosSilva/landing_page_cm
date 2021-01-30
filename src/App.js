bb4dec4c3126dd33129df8193dd50dccfee764c8
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-01-30 01:25:52 -0300

    ajusts eslint

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages';
 
-function App() {
-  return (
-    <Router>
-      <Home />
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Home />
+  </Router>
+
+);
 
 export default App;
